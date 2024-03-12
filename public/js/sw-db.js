f6f190a379394522ec0a34c643df369bf86e3631
erilvm <erika.nemesio2002@gmail.com>
2024-03-11 22:39:28 -0600

    first commit

---
+++ public/js/sw-db.js
@@ -0,0 +1,39 @@
+﻿const db = PouchDB('notas');
+
+function guardarNote(nota) {
+    nota._id = new Date().toISOString();
+    return db.put(nota).then(()=> {
+        self.registration.sync.register('nuevo-post');
+        const newResp = {ok:true,offline:true};
+        return new Response(JSON.stringify(newResp));
+    });
+}
+
+function postearNotes() {
+    const posteos = [];
+    
+    return db.allDocs({include_docs:true}).then(docs => {
+        
+        docs.rows.forEach(row => {
+            const doc = row.doc;
+            const data = {
+                title: doc.title,
+                text: doc.text,
+            }
+
+            const fetchProm = fetch('http://localhost:3001/api/note', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            }).then(resp => {
+                // console.log('borra el doc de indexDB')
+                console.log('resp ', resp.json())
+                return db.remove(doc);
+            });
+            posteos.push(fetchProm);
+        })
+        return Promise.all(posteos);
+    });
+}
